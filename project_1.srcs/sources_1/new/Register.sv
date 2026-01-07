af8a3ae61ee0486e4e95432084b439e4b4b08c40
Pradipgyawali897 <gyawalipradip846@gmail.com>
2026-01-08 00:14:24 +0545

    Creation of SISO Register SISO register was created and testbench is to be written.

---
+++ project_1.srcs/sources_1/new/Register.sv
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/07/2026 11:49:02 PM
+// Design Name: 
+// Module Name: Register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Register(
+    input logic clk,
+    input logic rst_n,
+    input logic si,
+    output logic so
+);
+
+    logic [3:0] q;
+    
+    always_ff @(posedge clk or negedge rst_n) begin 
+        if(!rst_n)
+            q <= 4'b0000;        
+        else
+            q <= {q[2:0], si};  
+    end
+        
+    assign so = q[3];
+    
+endmodule
+
