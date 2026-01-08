12fa35d19b514fac66bafd8a9660fe343c019f87
Pradipgyawali897 <gyawalipradip846@gmail.com>
2026-01-08 21:03:31 +0545

    Serial In Parallel Out Resister Implemented

---
+++ project_1.srcs/sources_1/new/PISO.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/08/2026 08:56:16 PM
+// Design Name: 
+// Module Name: PISO
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PISO(
+
+    );
+endmodule
---
+++ project_1.srcs/sources_1/new/SIFO.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/08/2026 08:55:34 PM
+// Design Name: 
+// Module Name: SIFO
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SIFO(
+
+    );
+endmodule
